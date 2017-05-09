cb19592b0671d587034b21a28269c9a0a221f5b9
Michael Paris <michael@scaphold.io>
2017-05-09 15:28:45 -0700

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,12 @@
 # /usr/bin/bash
+set -e
 
 echo 'Patching full package.json'
 npm version patch
 mv ../package.json ../package.json.tmp
 
 echo 'Swapping package.json'
-mv ../package.json.npm ../package.json
+mv ../package.npm.json ../package.json
 
 echo 'Patching swapped package.json'
 npm version patch
@@ -14,5 +15,5 @@ echo 'Publishing to NPM'
 npm publish
 
 echo 'Swapping back package.json'
-mv ../package.json ../package.json.npm
+mv ../package.json ../package.npm.json
 mv ../package.json.tmp ../package.json
