07666d047d3c4ede5ffcefb25f441f6c705539f8
Michael Paris <michael@scaphold.io>
2017-05-09 15:49:22 -0700

    publish update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,6 @@
 # /usr/bin/bash
 set -e
 
-echo 'Temporarily ignoring package.json'
-
-echo 'Stashing git'
-git stash
-
 echo 'Patching full package.json'
 npm version patch
 mv package.json package.json.tmp
@@ -13,24 +8,12 @@ mv package.json package.json.tmp
 echo 'Swapping package.json'
 mv package.npm.json package.json
 
-echo 'Stashing git'
-git stash
-
 echo 'Patching swapped package.json'
 npm version patch
 
 echo 'Publishing to NPM'
 npm publish
 
-echo 'Unstashing git'
-git stash drop
-git stash drop
-
 echo 'Swapping back package.json'
 mv package.json package.npm.json
 mv package.json.tmp package.json
-
-echo 'Removing temp .npmignore'
-cp .gitignore .gitignore.tmp
-head -n -3 .gitignore.tmp > .gitignore
-rm -f .gitignore.tmp
