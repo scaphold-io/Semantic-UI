07244d084dfbadbd8ff9addcaf87da5e85e7fd7f
Michael Paris <michael@scaphold.io>
2017-05-09 15:35:04 -0700

    updated ignore

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,12 @@
 # /usr/bin/bash
 set -e
 
+echo 'Temporarily ignoring package.json'
+
+echo 'package.json' >> .gitignore
+echo 'package.json.tmp' >> .gitignore
+echo '.gitignore' >> .gitignore
+
 echo 'Patching full package.json'
 npm version patch
 mv package.json package.json.tmp
@@ -17,3 +23,8 @@ npm publish
 echo 'Swapping back package.json'
 mv package.json package.npm.json
 mv package.json.tmp package.json
+
+echo 'Removing temp .npmignore'
+cp .gitignore .gitignore.tmp
+head -n -3 .gitignore.tmp > .gitignore
+rm -f .gitignore.tmp
